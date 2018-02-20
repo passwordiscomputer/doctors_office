667e401ffcf3b74f788d4d02b56f32d86f052170
alex ficklin and russell hofvendahl <russell@>
2018-02-20 10:55:11 -0800

    initalize patient spec pass.

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,11 +4,11 @@ require("doctor")
 class Patient
   attr_reader :name, :birth, :id, :doctor_id
 
- #  def initialize(attributes)
- #   @name = attributes[:name]
- #   @birth = attributes[:birth]
- #   DB.exec("INSERT INTO patients (name, birth) VALUES ('#{@name}', '#{@birth}')")
- #   result = DB.exec("SELECT id FROM patients WHERE name='#{@name}'")
- #   @id = result[0].fetch("id").to_i
- # end
+  def initialize(attributes)
+   @name = attributes[:name]
+   @birth = attributes[:birth]
+   DB.exec("INSERT INTO patients (name, birth) VALUES ('#{@name}', '#{@birth}')")
+   result = DB.exec("SELECT id FROM patients WHERE name='#{@name}'")
+   @id = result[0].fetch("id").to_i
  end
+end
